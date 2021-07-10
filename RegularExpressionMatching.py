7ec28462a2a41870320f62e6fe9cd6abdc2eeef0
robkpaul <rob.k.paul@gmail.com>
2021-07-10 09:48:30 -0700

    not a project

--- RegularExpressionMatching.py
+++
@@ -1,7 +0,0 @@
-import re
-
-phoneNumRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})|\((\d{3})\) (\d{3}-\d{4})')
-
-response = input("What\'s your phone number?")
-
-print(phoneNumRegex.search(response).groups()) 
